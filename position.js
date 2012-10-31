005907224e0680063969b9e90e8881c05c093e2a
John McKerrell <john@mckerrell.net>
2012-10-31 18:03:59 +0000

    Re-jigging so that request to mapme.at made in one place.

--- position.js
+++ position.js
@@ -3,13 +3,37 @@ var url = require("url");
 
 function checkPosition(mac,currentPosition,currentPlaceHash,response,connections)
 {    
+    // Add this connection to the array of connections
+    var connection = {};
+    var macConnections = connections[mac.toString()];
+    if (!macConnections) {
+        console.log("Setting mac connections");
+        macConnections = [];
+        connections[mac.toString()] = macConnections;
+    }
+    macConnections.push(connection);
+    connection['response'] = response;
+    connection['currentPosition'] = currentPosition;
+    connection['currentPlaceHash'] = currentPlaceHash;
+
+    // then straight away check the current position
+    requestCurrentPosition(mac,connections);
+}
+
+
+function requestCurrentPosition(mac,connections)
+{
+	//connects to map.me.at
+    // mapme.at/api/wheredial.csv?mac=value
+
+
     var options = {
         host: 'mapme.at',
         port: 80,
         path: '/api/wheredial.csv?mac='+mac,
-        method: 'GET'
+        method: 'POST'
     };
-
+    
     var req = http.get(options, function(res) {
         var pageData = "";
         res.setEncoding('utf8');
@@ -22,54 +46,10 @@ function checkPosition(mac,currentPosition,currentPlaceHash,response,connections
             var result = pageData.split(',');
             var livePosition = result[0];
             var livePlaceHash = result[1];
-            if(livePosition == currentPosition && livePlaceHash == currentPlaceHash)
-            {
-                console.log("No need for rotation, WhereDial will wait");
-                var connection = {};
-                var macConnections = connections[mac.toString()];
-                if (!macConnections) {
-                    console.log("Setting mac connections");
-                    macConnections = [];
-                    connections[mac.toString()] = macConnections;
-                }
-                macConnections.push(connection);
-                connection['response'] = response;
-                connection['currentPosition'] = currentPosition;
-                connection['currentPlaceHash'] = currentPlaceHash;
-
-                console.log("Number of WhereDials waiting:"+ Object.keys(connections).length);
-            }else
-            {
-                response.writeHead(200, {"Content-Type": "text/plain"});
-                response.write(pageData);
-                response.end();
-            }
+            updatePosition(mac,livePosition,livePlaceHash,null,connections);
         });
       });
-}
 
-
-function getCurrentPosition(mac)
-{
-	//connects to map.me.at
-    // mapme.at/api/wheredial.csv?mac=value
-
-    var options = {
-        host: url,
-        port: 80,
-        path: 'mapme.at/api/wheredial.csv?mac='+mac,
-        method: 'POST'
-    };
-
-    http.request(options, function(res) {
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log('BODY: ' + chunk);
-        });
-    }).end();
-	return 210;
 }
 
 function updatePosition(mac,position,placeHash,response,connections)
@@ -79,9 +59,11 @@ function updatePosition(mac,position,placeHash,response,connections)
     if(macConnections)
     {
         //response for the map.me.at API
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Done, WhereDial should now rotate to:" +position);
-        response.end();
+        if (response) {
+            response.writeHead(200, {"Content-Type": "text/plain"});
+            response.write("Done, WhereDial should now rotate to:" +position);
+            response.end();
+        }
 
         var updatedConnections = [];
         for (var i in macConnections) {
@@ -101,11 +83,14 @@ function updatePosition(mac,position,placeHash,response,connections)
     }else
     {
         console.log("Trying to update the position of ["+mac+"] to "+position+ " but WhereDial is not connected");
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("No WhereDial with this mac address connected");
-        response.end();
+        if (response) {
+            response.writeHead(200, {"Content-Type": "text/plain"});
+            response.write("No WhereDial with this mac address connected");
+            response.end();
+        }
     }
 }
 
 exports.checkPosition = checkPosition;
 exports.updatePosition = updatePosition;
+exports.requestCurrentPosition = requestCurrentPosition;
